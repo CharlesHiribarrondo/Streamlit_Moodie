918825dbc495a1a7cfc2121acd17b979f70a2d78
Charles Hiribarrondo <96682469+CharlesHiribarrondo@users.noreply.github.com>
2023-04-21 16:39:19 +0200

    Add files via upload

---
+++ Accueil.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("Moodie 🎬")
+
+st.subheader("Bienvenue chez Moodie !")
+st.write("Toi aussi, tu te lasses des recommandations de Netflix et cie ? Tu aimerais retrouver une démarche active, où tu trouves le film / la série à mater pour la soirée selon ton humeur ? Alors tu es à la bonne porte !")
+st.write("Notre humeur, nos envies changent quotidiennement. Nous pouvons très bien avoir envie d'un documentaire sur un sujet de société un soir, poursuivre notre crush série le lendemain, et chiller devant Dumb and Dumber le surlendemain. Et ça, pas sûr que Netflix le comprenne bien. En effet, les plateformes de streaming vous recommandent des films/séries selon votre historique de visionnage et l'évaluation de titres similaires. Problème, ces systèmes de recommandation ne pourront jamais intégrer vos envies fluctantes, et resteront bloqués dans le genre le plus représenté au cours d'une période donnée.")
+
+
+st.write("Chez Moodie, nous raisonnons différemment. Qui mieux que vous savez quel genre de films / séries vous souhaitez regarder le soir, en rentrant chez vous ? Nous sommes convaincus que la recommandation du soir ne doit pas dépendre des visionnages de la veille, mais de votre envie du moment. C'est pourquoi nous avons développé un système de recommandations selon votre humeur, et vos envies. Allez, c'est parti ! 😀")
